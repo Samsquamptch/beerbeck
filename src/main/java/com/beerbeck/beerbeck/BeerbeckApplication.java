97ac8097577b3bed047607d8f470701a4658812f
Samscranch <140660422+Samscranch@users.noreply.github.com>
2023-10-13 03:29:56 +0100

    Added Spring Boot Init Java 17 Gradle

---
+++ src/main/java/com/beerbeck/beerbeck/BeerbeckApplication.java
@@ -0,0 +1,13 @@
+package com.beerbeck.beerbeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerbeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerbeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beerbeck/beerbeck/BeerbeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerbeck.beerbeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerbeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
